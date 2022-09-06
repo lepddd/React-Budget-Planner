cae84cb1ae0b794c13dec07f09c0d2523bd676cd
Luiz Dornelas <luizeduardojf@hotmail.com>
2022-09-06 14:06:59 -0300

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import Budget from "./Components/Budget";
-import Remaning from "./Components/Remaning";
-import Spend from "./Components/Spend";
-import FilterInput from "./Components/FilterInput";
-import AddExpend from "./Components/AddExpend";
+import Header from "./Components/Header";
+import FilterBox from "./Components/FilterBox";
 import Expenses from "./Components/Expenses";
 import Modal from "./Components/Modal";
-import TitleSection from "./Components/TitleSection";
+import AddExpendBox from "./Components/AddExpendBox";
 
 function App() {
   return (
     <>
       <Modal />
       <div className="p-3 lg:px-6 lg:py-10">
-        <div className="flex flex-col gap-1 mb-4 lg:flex-row lg:gap-2">
-          <Budget />
-          <Remaning />
-          <Spend />
-        </div>
-
-        <div>
-          <TitleSection title={"Expenses"} />
-          <FilterInput />
-        </div>
-
-        <Expenses />
-
-        <div>
-          <TitleSection title={"Add Expenses"} />
-          <AddExpend />
-        </div>
+        <Header/>
+        <FilterBox/>
+        <Expenses />        
+        <AddExpendBox/>        
       </div>
     </>
   );
---
+++ src/Components/AddExpendBox.jsx
@@ -0,0 +1,13 @@
+import AddExpend from "./AddExpend";
+import TitleSection from "./TitleSection";
+
+const AddExpendBox = () => {
+  return (
+    <div>
+      <TitleSection title={"Add Expenses"} />
+      <AddExpend />
+    </div>
+  );
+};
+
+export default AddExpendBox;
